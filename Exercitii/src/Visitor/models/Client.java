f13bc8f3b602ebe9fb226ac2cbab79ecf744b042
Mihai Duminica <mihai.duminica@aquasoft.ro>
2025-04-28 19:12:25 +0300

    Visitor

---
+++ Exercitii/src/Visitor/models/Client.java
@@ -0,0 +1,12 @@
+package Visitor.models;
+
+import lombok.Data;
+
+@Data
+public abstract class Client {
+
+    private final String name;
+    private final String address;
+    private final String number;
+
+    public abstract void accept(Visitor visitor);
