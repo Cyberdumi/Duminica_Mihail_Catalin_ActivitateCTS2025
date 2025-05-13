2a402b2a76707660cda118436dad4a1da2add5a7
Mihai Duminica <mihaicatalin.duminica@gmail.com>
2025-05-13 19:38:40 +0300

    Memento

---
+++ Exercitii/src/Memento/TextArea.java
@@ -0,0 +1,39 @@
+package Memento;
+
+import java.awt.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class TextArea {
+
+    private String text;
+
+    public void set(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Memento takeSnapshot() {
+        return new Memento(this.text);
+    }
+
+    public void restore(Memento memento) {
+        this.text = memento != null ? memento.getSavedText() : null;
+    }
+
+    public static class Memento {
+        private final String text;
+
+        private Memento(String textToSave) {
+            text = textToSave;
+        }
+
+        private String getSavedText() {
+            return text;
+        }
+    }
+
+}
